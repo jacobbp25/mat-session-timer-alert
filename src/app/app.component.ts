fbed1c2edc1927ec5eedaf79b742dc466f08a93a
Feuerbach, Jacob D <jacob.feuerbach@gmail.com>
2019-08-15 22:59:40 -0500

    Fixed client demo side to work with timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SessionTimerService } from 'session-timer-alert';
 
 @Component({
   selector: 'seademo-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mat-session-expiration-alert-demo';
+  alertAt = 15;
+  startTimer = true;
+
+  constructor(public sessionTimer: SessionTimerService) {}
+  increase() {
+    this.alertAt++;
+  }
+  toggletimer() {
+    this.startTimer = !this.startTimer;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatDialogModule } from '@angular/material/dialog';
 
-import { SessionTimerAlertModule } from '../../dist/session-timer-alert'; //'session-timer-alert';
+import { SessionTimerAlertModule, SessionInteruptService } from '../../dist/session-timer-alert'; //'session-timer-alert';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppSessionInteruptService } from './services/app-session-interupt.service';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, SessionTimerAlertModule, MatDialogModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    HttpClientModule,
+    SessionTimerAlertModule.forRoot({ totalMinutes: 0.5 })
+  ],
+  providers: [{ provide: SessionInteruptService, useClass: AppSessionInteruptService }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
