93c55bbb59f57fb6da41d3951263b170619fcc98
Jacob Feuerbach <jacob-feuerbach@uiowa.edu>
2020-03-26 15:25:11 -0500

    Updated library to use more specific matdialog

--- projects/session-timer-alert/src/lib/session-timer-alert.module.ts
+++ projects/session-timer-alert/src/lib/session-timer-alert.module.ts
@@ -1,21 +1,31 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MatDialogModule } from '@angular/material';
-import { MatButtonModule } from '@angular/material/button';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgModule, ModuleWithProviders } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatButtonModule } from "@angular/material/button";
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
 
-import { SessionTimerService } from './services/session-timer.service';
-import { SessionTimerHttpInterceptor } from './services/session-timer-http-interceptor';
-import { SessionTimerAlertComponent, MatSessionTimerAlertDialog } from './components/session-timer-alert/session-timer-alert.component';
-import { SessionInteruptService } from './services/session-interupt.service';
-import { SessionExpirationConfig, ConfigToken } from './models/session-expiration-config';
+import { SessionTimerService } from "./services/session-timer.service";
+import { SessionTimerHttpInterceptor } from "./services/session-timer-http-interceptor";
+import {
+  SessionTimerAlertComponent,
+  MatSessionTimerAlertDialog
+} from "./components/session-timer-alert/session-timer-alert.component";
+import { SessionInteruptService } from "./services/session-interupt.service";
+import {
+  SessionExpirationConfig,
+  ConfigToken
+} from "./models/session-expiration-config";
 
 @NgModule({
   declarations: [SessionTimerAlertComponent, MatSessionTimerAlertDialog],
   imports: [CommonModule, MatDialogModule, MatButtonModule],
   providers: [
     SessionTimerService,
-    { provide: HTTP_INTERCEPTORS, useClass: SessionTimerHttpInterceptor, multi: true },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: SessionTimerHttpInterceptor,
+      multi: true
+    },
     SessionInteruptService
   ],
   exports: [SessionTimerAlertComponent, MatSessionTimerAlertDialog],
